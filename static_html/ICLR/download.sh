9993155473a5706312f989c2809c7e253f5efd9c
Kevin Coakley <kcoakley@sdsc.edu>
2026-03-01 13:29:12 -0800

    Fixed download url for ICLR 2025 static html
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- static_html/ICLR/download.sh
+++ static_html/ICLR/download.sh
@@ -2,4 +2,4 @@ wget https://iclr.cc/archive/2014/old-site/program-details/conference-program.ht
 wget https://iclr.cc/archive/www/doku.php%3Fid=iclr2015:accepted-main.html -O 2015.html
 wget https://iclr.cc/archive/www/doku.php%3Fid=iclr2016:accepted-main.html -O 2016.html
 wget https://proceedings.iclr.cc/paper_files/paper/2024 -O 2024.html
-wget https://proceedings.iclr.cc/paper_files/paper/2024 -O 2024.html
+wget https://proceedings.iclr.cc/paper_files/paper/2025 -O 2025.html
